b75bd76e09f64c5bc8ec309310b6a15f11c2f44e
Daniel Tebar <dtebar@gmail.com>
2024-12-19 09:48:41 -0500

    SecondCommit

--- mtb_v4_settings/settings.py
+++ mtb_v4_settings/settings.py
@@ -13,57 +13,56 @@ SECRET_KEY = 'django-insecure-^yhjhum@rt2o(@hzfm@m34oym5g9!$00j8*wd01a&ge^x_sy5e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1" ]
+ALLOWED_HOSTS = [ "localhost", "127.0.0.1" ]
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join( BASE_DIR, 'staticfiles' )
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    # ... other directories if needed
+  os.path.join( BASE_DIR, 'static' ),
+  # ... other directories if needed
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join( BASE_DIR, 'media' )
 MEDIA_URL = '/media/'
 
-
 INSTALLED_APPS = [
- 'django.contrib.admin',
- 'django.contrib.auth',
- 'django.contrib.contenttypes',
- 'django.contrib.sessions',
- 'django.contrib.messages',
- 'django.contrib.staticfiles',
- 'mtb_v4_app',
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.messages',
+  'django.contrib.staticfiles',
+  'mtb_v4_app',
 ]
 
 MIDDLEWARE = [
- 'django.middleware.security.SecurityMiddleware',
- 'django.contrib.sessions.middleware.SessionMiddleware',
- 'django.middleware.common.CommonMiddleware',
- 'django.middleware.csrf.CsrfViewMiddleware',
- 'django.contrib.auth.middleware.AuthenticationMiddleware',
- 'django.contrib.messages.middleware.MessageMiddleware',
- 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  'django.middleware.security.SecurityMiddleware',
+  'django.contrib.sessions.middleware.SessionMiddleware',
+  'django.middleware.common.CommonMiddleware',
+  'django.middleware.csrf.CsrfViewMiddleware',
+  'django.contrib.auth.middleware.AuthenticationMiddleware',
+  'django.contrib.messages.middleware.MessageMiddleware',
+  'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'mtb_v4_settings.urls'
 
 TEMPLATES = [
- {
-  'BACKEND' : 'django.template.backends.django.DjangoTemplates',
-  'DIRS': [BASE_DIR / 'templates'],
-  'APP_DIRS': True,
-  'OPTIONS' : {
-   'context_processors': [
-    'django.template.context_processors.debug',
-    'django.template.context_processors.request',
-    'django.contrib.auth.context_processors.auth',
-    'django.contrib.messages.context_processors.messages',
-   ],
+  {
+    'BACKEND' : 'django.template.backends.django.DjangoTemplates',
+    'DIRS'    : [ BASE_DIR / 'templates' ],
+    'APP_DIRS': True,
+    'OPTIONS' : {
+      'context_processors': [
+        'django.template.context_processors.debug',
+        'django.template.context_processors.request',
+        'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+      ],
+    },
   },
- },
 ]
 
 WSGI_APPLICATION = 'mtb_v4_settings.wsgi.application'
@@ -71,40 +70,40 @@ WSGI_APPLICATION = 'mtb_v4_settings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 #
-# DATABASES = {
-#  'default': {
-#   'ENGINE': 'django.db.backends.sqlite3',
-#   'NAME'  : BASE_DIR / 'db.sqlite3',
-#  }
-# }
-
 DATABASES = {
  'default': {
-  'ENGINE'  : 'django.db.backends.postgresql',  # Or 'django.db.backends.postgresql_psycopg2'
-  'NAME'    : 'mtb_v4_pgdb',
-  'USER'    : 'postgres',
-  'PASSWORD': 'postgres',
-  'HOST'    : 'localhost',  # Or the IP address of your PostgreSQL server
-  'PORT'    : '5432',
+  'ENGINE': 'django.db.backends.sqlite3',
+  'NAME'  : BASE_DIR / 'db.sqlite3',
  }
 }
+
+# DATABASES = {
+#   'default': {
+#     'ENGINE'  : 'django.db.backends.postgresql',  # Or 'django.db.backends.postgresql_psycopg2'
+#     'NAME'    : 'mtb_v4_pgdb',
+#     'USER'    : 'postgres',
+#     'PASSWORD': 'postgres',
+#     'HOST'    : 'localhost',  # Or the IP address of your PostgreSQL server
+#     'PORT'    : '5432',
+#   }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
- {
-  'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
- },
- {
-  'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
- },
- {
-  'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
- },
- {
-  'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
- },
+  {
+    'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+  },
 ]
 
 # Internationalization
@@ -125,7 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 # CRISPY_TEMPLATE_PACK = 'uni_form'
 # CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap3"
 # CRISPY_TEMPLATE_PACK = "bootstrap3"
